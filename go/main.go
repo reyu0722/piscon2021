1f136f435e6d69fdfd3c79cd963fbf1d6259125d
reyu <ka15sugar@gmail.com>
2021-07-18 00:22:59 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -922,7 +922,7 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := tx.Select(&itemDetailDBs,
-			queryStr+"WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY created_at DESC, id DESC LIMIT ?",
+			queryStr+"WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND (`i.created_at` < ?  OR (`i.created_at` <= ? AND `id` < ?)) ORDER BY created_at DESC, id DESC LIMIT ?",
 			user.ID,
 			user.ID,
 			ItemStatusOnSale,
