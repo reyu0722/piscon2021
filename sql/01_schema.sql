02f5a3a1a3ea92484cbdd615522487f66d474b45
reyu <ka15sugar@gmail.com>
2021-07-20 01:08:33 +0900

    test

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE `items` (
   `category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_seller_id (`seller_id`),
+  INDEX idx_created_at_id(`created_at`, `id`),
   INDEX idx_seller_created_at_id(`seller_id`, `created_at` DESC, `id` DESC),
   INDEX idx_buyer_created_at_id(`buyer_id`, `created_at` DESC, `id` DESC)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
