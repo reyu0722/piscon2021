d178aa8995ea2bd4685a30bd0ba729bfb742abbd
reyu <ka15sugar@gmail.com>
2021-07-22 02:01:28 +0900

    test

--- go/main.go
+++ go/main.go
@@ -1447,6 +1447,7 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 		outputErrorMsg(w, http.StatusNotFound, "user not found")
 		return
 	}
+	/*
 	canUseCache := false
 	item := ItemDetailDB{}
 	itemCache, ok := itemDetailCache[itemID]
@@ -1460,6 +1461,7 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !canUseCache {
+		*/
 		queryStr := `SELECT i.*, 
 			u.id as "seller.id",
 			u.account_name as "seller.account_name",
@@ -1476,7 +1478,7 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 			left outer join transaction_evidences t on t.item_id=i.id
 			left outer join shippings s on s.transaction_evidence_id=t.id 
 		`
-		item = ItemDetailDB{}
+		item := ItemDetailDB{}
 		err = dbx.Get(&item, queryStr+" WHERE i.id = ?", itemID)
 		if err == sql.ErrNoRows {
 			outputErrorMsg(w, http.StatusNotFound, "item not found")
@@ -1494,7 +1496,7 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 			userSimpleCache[item.BuyerID].Set(*item.Buyer)
 		}
 		*/
-	}
+	//}
 
 	category, err := getCategoryByID(dbx, item.CategoryID)
 	if err != nil {
