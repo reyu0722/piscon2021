eab96429d287c198c5ed61f0257b07140526f88a
reyu <ka15sugar@gmail.com>
2021-07-22 02:49:58 +0900

    add dbx settings

--- go/main.go
+++ go/main.go
@@ -342,6 +342,9 @@ func main() {
 
 	mux := goji.NewMux()
 
+	dbx.SetMaxIdleConns(1024)
+	dbx.SetConnMaxLifetime(0)
+
 	http.DefaultTransport.(*http.Transport).MaxIdleConns = 0
 	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 1024
 	http.DefaultTransport.(*http.Transport).ForceAttemptHTTP2 = true
